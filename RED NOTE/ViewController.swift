571de44a6590b30de57d6bc28ba857740cd6b446
orekhova <orekhova@msk-wifi-b22p8-j_orekhova-j.orekhova.mail.msk>
2018-11-06 20:10:10 +0300

    Initial Commit

---
+++ RED NOTE/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RED NOTE
+//
+//  Created by orekhova on 06/11/2018.
+//  Copyright © 2018 Julika Orekhova Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
