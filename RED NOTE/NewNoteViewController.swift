6be524b11da1263c7260586522a8d912f49e2af1
orekhova <orekhova@jorekhova-3.local>
2018-11-06 22:16:12 +0300

    Create 1st version of our project

---
+++ RED NOTE/NewNoteViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewNoteViewController.swift
+//  RED NOTE
+//
+//  Created by orekhova on 06/11/2018.
+//  Copyright © 2018 Julika Orekhova Corp. All rights reserved.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
